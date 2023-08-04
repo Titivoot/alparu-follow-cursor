ddacd5b5453c0eac8e6f9d540103bb2ae2ba95d1
Titivoot Pangrit <tanlnwtitivoot@gmail.com>
2023-08-04 15:13:13 +0700

    Initial commit

---
+++ popup.js
@@ -0,0 +1,23 @@
+const slider = document.querySelector('.slider');
+const checkbox = document.querySelector('input[type="checkbox"]');
+
+function setSliderBackground(checked) {
+  slider.style.backgroundColor = checked ? '#000' : '#B0B0B0';
+}
+
+function toggleCheckbox() {
+  const checked = checkbox.checked;
+  setSliderBackground(checked);
+  chrome.storage.local.set({ checked });
+  chrome.tabs.reload();
+}
+
+function loadToggleStateFromStorage() {
+  chrome.storage.local.get('checked', ({ checked }) => {
+    checkbox.checked = checked;
+    setSliderBackground(checked);
+  });
+}
+
+checkbox.addEventListener('change', toggleCheckbox);
+loadToggleStateFromStorage();
